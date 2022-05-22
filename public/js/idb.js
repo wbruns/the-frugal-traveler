f923c1fe8c8b0df35d8d81904c02ac7541a0a903
wbruns <wbruns07@gmail.com>
2022-05-21 23:31:48 -0500

    added offline data persistence for transactions with indexedDB

---
+++ public/js/idb.js
@@ -0,0 +1,87 @@
+// create variable to hold db connection
+let db;
+// establish a connection to IndexedDB databas called 'frugal-traveler' and set it to version 1
+const request = indexedDB.open('frugal_traveler', 1);
+
+// this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
+request.onupgradeneeded = function(event) {
+    // save a reference to the database 
+    const db = event.target.result;
+    // create an object store (table) called `transactions`, set it to have an auto incrementing primary key of sorts 
+    db.createObjectStore('transactions', { autoIncrement: true });
+  };
+
+  // upon a successful
+request.onsuccess = function(event) {
+    // when db is successfully created with its object store (from onupgradedneeded event above)
+    // or simply established a connection, save reference to db in global variable
+    db = event.target.result;
+
+    // check if app is online, if yes run uploadTransaction() function to send all local db data to api
+    if (navigator.onLine) {
+        uploadTransactions();
+    }
+};
+
+request.onerror = function(event) {
+    // log error here
+    console.log(event.target.errorCode);
+};
+
+// this function will be executed if we attempt to submit a new transaction and there's no internet connection
+function saveRecord(record) {
+    // open a new transaction with the database with read and write permissions
+    const transaction = db.transaction(['transactions'], 'readwrite');
+
+    // access the object store for `transactions`
+    const transactionObjectStore = transaction.objectStore('transactions');
+
+    // add record to your store with add method
+    transactionObjectStore.add(record);
+}
+
+function uploadTransactions() {
+    // open a transaction on your db
+    const transaction = db.transaction(['transactions'], 'readwrite');
+
+    // access your object store
+    const transactionObjectStore = transaction.objectStore('transactions');
+
+    // get all records from store and set to a variable
+    const getAll = transactionObjectStore.getAll();
+
+    // upon a successful .getAll() execution, run this function
+    getAll.onsuccess = function() {
+        // if there was data in indexedDb's store, let's send it to the api server
+        if (getAll.result.length > 0 ) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, tex/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+                    // open one more transaction
+                    const transaction = db.transaction(['transactions'], 'readwrite');
+                    // access the transactions object store
+                    const transactionObjectStore = transaction.objectStore('transactions');
+                    // clear all items in your store
+                    transactionObjectStore.clear();
+
+                    alert('All saved transactions have been submitted!');
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    };
+}
+
+// listen for app coming back online
+window.addEventListener('online', uploadTransactions);
